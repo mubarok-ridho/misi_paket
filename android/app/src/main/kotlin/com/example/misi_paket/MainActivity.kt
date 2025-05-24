e293d69284555abedf9124d40785483bea72b13a
Ridho Mubarok <dhxo.pyprogram@gmail.com>
2025-05-24 14:26:39 +0700

    Initial commit - setup Flutter project with customer screen

---
+++ android/app/src/main/kotlin/com/example/misi_paket/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.misi_paket
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/customer.dart';
+import 'screens/owner.dart';
+import 'screens/courier.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Rental App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: CustomerHome(), // langsung ke halaman Customer
+    );
+  }
+}
